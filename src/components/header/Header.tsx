c51139308cd33c2c1b0d38aecc8db20427fca68d
Yacoub-Gamil <yacoub-gamil@hotmail.com>
2025-01-31 21:28:29 +0200

    added header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,20 @@
-import Link from "next/link";
 import React from "react";
+import Link from "../link/Link";
+import { Routes } from "@/constants/enums";
+import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <header>
-      <div className=" container">
-        <Link href={"/"}></Link>
+    <header className=" py-4 md:py-6">
+      <div className=" container flex items-center justify-between">
+        <Link
+          href={Routes.ROOT}
+          className=" text-primary font-semibold text-2xl"
+        >
+          {" "}
+          🍕Pizza
+        </Link>
+        <Navbar />
       </div>
     </header>
   );
